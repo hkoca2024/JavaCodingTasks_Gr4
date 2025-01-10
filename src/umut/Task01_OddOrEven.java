a71ed025d0aba5ae38adb9c1229aba3bb48c3b1e
umutuhu <uhumut99@gmail.com>
2025-01-10 23:10:02 +0300

    Task01 OddorEven added by umut

---
+++ src/umut/Task01_OddOrEven.java
@@ -0,0 +1,23 @@
+package umut;
+
+public class Task01_OddOrEven {
+    /*
+    Write a method that can identify  a given number is even or odd.
+
+   EX:
+
+          identify(5) --> "Odd"
+          identify(6) --> "Even"
+ */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * returns the gıven number ıf ıt ıs odd or even
+     * @param number
+     */
+    public static void oddOrEven(int number){
+
+    }
+}
