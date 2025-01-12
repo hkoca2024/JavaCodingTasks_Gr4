ed2d06c2a3b1ae3e736e316c4dfdeadaa6f5b68b
meltemba <meltembykgz@gmail.com>
2025-01-12 14:34:40 +0300

    Task01 OddOrEven added by mltem

---
+++ src/meltem/Task01_oddOrEven.java
@@ -0,0 +1,22 @@
+package meltem;
+
+public class Task01_oddOrEven {
+    /*
+       Write a method that can identify  a given number is even or odd.
+
+      EX:
+
+             identify(5) --> "Odd"
+             identify(6) --> "Even"
+    */
+    public static void main(String[] args) {
+
+    }
+    /**
+     * returns the gıven number ıf ıt ıs odd or even
+     * @param number
+     */
+    public static void oddOrEven(int number){
+
+    }
+}
