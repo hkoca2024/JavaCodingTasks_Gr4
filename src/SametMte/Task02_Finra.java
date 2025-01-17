dede9521c4290e424ecc11ae21b6a4385e5002fe
Kritokunn <abdulsametmete@gmail.com>
2025-01-17 22:58:37 +0300

    Task02_Finra complated Explanation added.

--- src/SametMte/Task02_Finra.java
+++ src/SametMte/Task02_Finra.java
@@ -1,9 +1,23 @@
 package SametMte;
 
 public class Task02_Finra {
+    /*
+    FINRA:
+
+        Write a function that prints out the numbers from 1 to 30 but for numbers that are a multiple of 3, print "FIN"
+        instead of the number, and for numbers that are a multiple of 5, print "RA" instead of the number.
+        For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the number.
+     */
     public static void main(String[] args) {
         Finra();
     }
+
+    /**
+     * if numbers that are a multiple of 3 print FIN,
+     * if numbers that are a multiple of 5 print RA
+     * if numbers that are a multiple of 3 and 5 print FINRA
+     * else print the number
+     */
     public static void Finra(){
         for (int i = 1; i <= 30; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
