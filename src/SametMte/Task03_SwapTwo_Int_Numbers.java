2429a611524f5b6680f3ae3f67393469df07c3ad
Kritokunn <abdulsametmete@gmail.com>
2025-01-31 22:22:56 +0300

    Task03_SwapTwo_Int_Numbers fixed the method name

--- src/SametMte/Task03_SwapTwo_Int_Numbers.java
+++ src/SametMte/Task03_SwapTwo_Int_Numbers.java
@@ -10,10 +10,10 @@ public class Task03_SwapTwo_Int_Numbers {
         int num2=10;
         System.out.println("original\n"+"Num1 : "+num1+"\nNum2 : "+num2);
         System.out.println("-------------------------------------");
-        String result1 = SwapNumbers1(num1, num2);
+        String result1 = swapNumbers1(num1, num2);
         System.out.println(result1);
         System.out.println("-------------------------------------");
-        String result2 = SwapNumbers2(num1, num2);
+        String result2 = swapNumbers2(num1, num2);
         System.out.println(result2);
     }
     // first method
@@ -23,7 +23,7 @@ public class Task03_SwapTwo_Int_Numbers {
      * @param num2 takes given second value
      * @return retun num1 and num2 value
      */
-    public static String SwapNumbers1(int num1,int num2) {
+    public static String swapNumbers1(int num1,int num2) {
         int temp = num1;
         num1 = num2;
         num2 = temp;
@@ -38,10 +38,10 @@ public class Task03_SwapTwo_Int_Numbers {
      * @param num2 takes given second value
      * @return retun num1 and num2 value
      */
-    public static String SwapNumbers2(int num1,int num2) {
-        num1= num1+num2;
-        num2= num1-num2;
-        num1= num1-num2;
+    public static String swapNumbers2(int num1,int num2) {
+        num1= num1+num2; // num1= 15+30
+        num2= num1-num2; // num2= 45-30
+        num1= num1-num2; // num1= 45-15
         return "second method\n"+"Num1 : "+num1+"\nNum2 : "+num2;
     }
 }
