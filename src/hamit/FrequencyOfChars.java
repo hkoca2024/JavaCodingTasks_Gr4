ba5fd1454abc5bcf19aed0b856048f84d141fa43
90542 <hamit.koca@gmail.com>
2025-02-08 15:14:32 +0300

    frequencyOfChars code is added.

---
+++ src/hamit/FrequencyOfChars.java
@@ -0,0 +1,40 @@
+package hamit;
+
+public class FrequencyOfChars {
+    public static void main(String[] args) {
+
+        String str="AAABBCCCCDDDEEE";
+
+        frequencyOfChars(str);
+    }
+
+    /**
+     *
+     * @param str this method count the frequency of each character and return each character and frequency
+     *       side by side
+     */
+
+
+    public static void frequencyOfChars(String str) {
+        StringBuilder newStr=new StringBuilder();
+        //String newStr="";
+        int count=1;
+        for (int i = 0; i <str.length()-1 ; i++) {// to avoid boundary problem it is used
+            if(str.charAt(i)==str.charAt(i+1)){  // str.length()-1 instead of str.length()
+                if(count==1){
+                    newStr.append(str.charAt(i));// at first we add character to string
+                }
+                count++;//  count  equals character
+                if(i==str.length()-2){
+                    newStr.append(count);//  when i  comes to last element due to no changing
+                }                        //  java doesn't go else body and count is added here to string
+            }else{
+                newStr.append(count);//  when equals character finish count is added to string
+                count=1;
+            }
+
+        }
+        System.out.println(str);//AAABBCCCCDDDEEE
+        System.out.println(newStr);// output A3B2C4D3E3
+    }
+}
